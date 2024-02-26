80342cd29f2ab7800a2625ffa0d3b3017e46bfdc
Ricky Garcia <rg3646@outlook.com>
2024-02-25 19:20:59 -0700

    adds solution

--- Questions/BuildingsWithOceanView/BuildingsWithOceanView.spec.ts
+++ Questions/BuildingsWithOceanView/BuildingsWithOceanView.spec.ts
@@ -2,6 +2,8 @@ import findBuildings from "./BuildingsWithOceanView";
 
 describe('Buildings with an ocean view', () => {
     test('checks to see which building have view', () => {
-        expect(findBuildings([4,2,3,1])).toBe([0,2,3])
+        expect(findBuildings([4,2,3,1])).toEqual([0,2,3])
+        expect(findBuildings([4,3,2,1])).toEqual([0,1,2,3])
+        expect(findBuildings([1,3,2,4])).toEqual([3])
     })
 })
--- Questions/BuildingsWithOceanView/BuildingsWithOceanView.ts
+++ Questions/BuildingsWithOceanView/BuildingsWithOceanView.ts
@@ -1,5 +1,23 @@
-
 export default function findBuildings(heights: number[]): number[] {
+    let buildingsWithView: number[] = [];
+    let tallestSeen = 0;
 
-    return []
-}
+    for(let i = heights.length - 1; i >= 0; i--) {
+
+        if(heights[i] > tallestSeen) {
+            buildingsWithView.push(i);
+            tallestSeen = heights[i]
+        }
+    }
+
+    let left = 0;
+    let right = buildingsWithView.length - 1;
+
+    while(left < right) {
+        [buildingsWithView[left], buildingsWithView[right]] = [buildingsWithView[right], buildingsWithView[left]] 
+        left++
+        right--
+    }
+
+    return buildingsWithView
+};
