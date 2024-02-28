f2a2307cbea214655624e59c635d844fb0b40083
Ricky Garcia <rg3646@outlook.com>
2024-02-27 21:59:12 -0700

    Minor Edit

--- Questions/RandomPickWithWeight/RandomPickWithWeight.ts
+++ Questions/RandomPickWithWeight/RandomPickWithWeight.ts
@@ -1,15 +1,32 @@
 export default class Solution {
+    private prefixSums: number[]
+    private totalSum: number
+
     constructor(w: number[]) {
-        
+        this.prefixSums = []
+        let prefixSum = 0
+        for (let weight of w) {
+            prefixSum += weight;
+            this.prefixSums.push(prefixSum)
+        }
+        this.totalSum = prefixSum
     }
 
     pickIndex(): number {
-        return 0
+        const target = Math.floor(Math.random() * this.totalSum) + 1
+        let low = 0
+        let high = this.prefixSums.length - 1
+
+        while (low < high) {
+            const mid = Math.floor((low + high) / 2)
+            if (this.prefixSums[mid] < target) {
+                low = mid + 1
+            } else {
+                high = mid
+            }
+        }
+        return low
     }
 }
 
-/**
- * Your Solution object will be instantiated and called as such:
- * var obj = new Solution(w)
- * var param_1 = obj.pickIndex()
- */
+let solution = new Solution([1, 3]);
